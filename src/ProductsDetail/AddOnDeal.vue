d5ca0eea4801496b3bd4b3bcf98d0875bf1e3307
Mohammed <mumo7038@gmail.com>
2023-03-03 22:57:27 +0800

    first commit

--- src/ProductsDetail/AddOnDeal.vue
+++ src/ProductsDetail/AddOnDeal.vue
@@ -60,8 +60,16 @@
             </div>
           </v-card>
         </v-col>
+        <!-- See more  -->
         <v-col cols="" class="pa-2 text-end">
-          <v-btn text depressed class="text-for-all"> رؤية الكل </v-btn>
+          <v-btn
+            @click.stop="dialog = true"
+            text
+            depressed
+            class="text-for-all"
+          >
+            رؤية الكل
+          </v-btn>
         </v-col>
         <v-divider vertical></v-divider>
         <!-- <v-spacer></v-spacer> -->
@@ -174,7 +182,11 @@
             tile
             height="100%"
           >
-            <v-card rounded="lg" class="text-center pa-1">
+            <v-card
+              @click.stop="dialog = true"
+              rounded="lg"
+              class="text-center pa-1"
+            >
               <v-card-text class="card-text pa-0">
                 المزيد
                 <span class="mr-1">1</span>
@@ -184,10 +196,10 @@
             </v-card>
           </v-card>
         </v-col>
+        <!-- price total and price befor  -->
         <v-col cols="12" class="pa-2 col-price">
           <v-row>
             <v-col cols="6">
-              <!-- price total and price befor  -->
               <v-card-text class="pa-0 total-price">
                 المجموع :
                 <span class="pa-0 befor-price">75 ريال</span>
@@ -208,17 +220,170 @@
           </v-row>
         </v-col>
       </v-row>
+      <!--  -->
+      <v-dialog
+        class="rounded-0"
+        persistent
+        no-click-animation
+        v-model="dialog"
+        width="1000"
+      >
+        <v-toolbar fixed>
+          <v-toolbar-title>
+            المحدد
+            <span class="mr-2">
+              <span class="deep-orange--text">0</span> /
+              {{ getCarInfo.length }}
+            </span>
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn depressed dark class="red darken-1 text-for-all">
+            إضافة الى السلة
+          </v-btn>
+          <v-btn @click="dialog = false" icon class="mr-2">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-card flat tile height="590" color="grey lighten-2 overflow-y-auto">
+          <v-row no-gutters>
+            <v-col
+              cols="12"
+              v-for="(Product, index) in getCarInfo"
+              :key="index"
+              class="pa-1"
+            >
+              <v-card
+                width="100%"
+                style="overflow: hidden; background-color: #fff"
+                flat
+              >
+                <v-row no-gutters align="center">
+                  <!-- select product -->
+                  <v-checkbox
+                    v-model="enabled"
+                    hide-details
+                    class="shrink mr-2 mt-0"
+                    dense
+                  ></v-checkbox>
+                  <!-- Product Img and name  -->
+                  <v-col cols="8" md="6" lg="6" sm="6">
+                    <v-row no-gutters align="center">
+                      <a
+                        router-link
+                        :to="{
+                          name: 'ShowTheProduct',
+                          params: {
+                            carName: Product.name,
+                            carShape: Product.Shape,
+                            carId: Product.id,
+                            Company: Product.folder,
+                          },
+                        }"
+                      >
+                        <v-img
+                          width="130"
+                          height="100"
+                          contain
+                          :src="getimageUrl(Product.folder, Product.image)"
+                        ></v-img>
+                      </a>
+                      <v-col>
+                        <v-card-text
+                          :to="{
+                            name: 'ShowTheProduct',
+                            params: {
+                              carName: Product.name,
+                              carShape: Product.Shape,
+                              carId: Product.id,
+                              Company: Product.folder,
+                            },
+                          }"
+                          style="
+                            display: -webkit-box;
+                            -webkit-line-clamp: 2;
+                            -webkit-box-orient: vertical;
+                            overflow: hidden;
+                          "
+                          class="card-text py-1 pb-0 pa-2"
+                        >
+                          {{ Product.name }} {{ Product.company }}
+                          {{ Product.name }} {{ Product.company }}
+                          {{ Product.name }} {{ Product.company }}
+                          {{ Product.name }} {{ Product.company }}
+                          {{ Product.name }} {{ Product.company }}
+                          {{ Product.name }} {{ Product.company }}
+                          {{ Product.name }} {{ Product.company }}
+                          {{ Product.name }} {{ Product.company }}
+                          {{ Product.name }}
+                          {{ Product.company }}
+                        </v-card-text>
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                  <v-col>
+                    <v-card-actions class="justify-center pa-0">
+                      <v-btn depressed text class="text-for-all" color="grey darken-2">
+                        تحديد الطلب
+                        <v-icon>mdi-menu-down</v-icon>
+                      </v-btn>
+                    </v-card-actions>
+                  </v-col>
+                  <v-col cols="">
+                    <!-- Product price -->
+                    <strong
+                      class="PriceAfter pb-0 grey--text text-decoration-line-through text-center py-1 pa-2 text-truncate"
+                    >
+                      {{ Product.payment }}
+                      <small class="text-truncate">ريال</small>
+                    </strong>
+                    <strong
+                      class="PriceAfter pt-0 red--text py-1 pa-2 text-center text-truncate"
+                    >
+                      {{ Product.payment }}
+                      <small class="text-truncate">ريال</small>
+                    </strong>
+                  </v-col>
+                </v-row>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-dialog>
     </v-container>
   </div>
 </template>
 <script>
+import VerifiedCar from "../data-json/All-Car.json";
 export default {
   name: "SameWonerProducts",
   components: {},
   data() {
-    return {};
+    return {
+      dialog: false,
+      VerifiedCar,
+      carName: this.$route.params.carName,
+      carId: this.$route.params.carId,
+      CarShape: this.$route.params.CarShape,
+      Company: this.$route.params.Company,
+    };
   },
-  // this is help full to call the image inside folder and inject to the src
+  methods: {
+    getimageUrl(FolderName, ImageName) {
+      let image = require.context("@/assets/");
+      return image("./" + FolderName + "/" + ImageName);
+    },
+  },
+  computed: {
+    getCarInfo() {
+      let GetCarVerified = [];
+      for (let i = 0; i < this.VerifiedCar.length; i++) {
+        if (this.VerifiedCar[i].Vip == true) {
+          GetCarVerified.push(this.VerifiedCar[i]);
+        }
+      }
+      return GetCarVerified;
+    },
+  },
 };
 </script>
 
@@ -257,49 +422,54 @@ export default {
       font-size: 17px;
     }
   }
-  .how-many-percent {
-    position: absolute;
-    top: 0;
-    right: 0;
-    clip-path: polygon(25% 0%, 100% 0%, 100% 100%, 25% 100%, 0% 50%);
-    background-color: #ef5350;
-    padding: 0px 3px 0px 8px;
-    color: white;
-    @media (max-width: 960px) {
-      // clip-path: none !important;
-      font-size: 12px !important;
-      // padding: 0px 2px !important;
-      // border-radius: 3px !important;
-    }
+}
+.text-for-all {
+  font-family: $fontfamliy3;
+  letter-spacing: 0;
+  font-size: 14px;
+}
+.how-many-percent {
+  position: absolute;
+  top: 0;
+  right: 0;
+  clip-path: polygon(25% 0%, 100% 0%, 100% 100%, 25% 100%, 0% 50%);
+  background-color: #ef5350;
+  padding: 0px 3px 0px 8px;
+  color: white;
+  @media (max-width: 960px) {
+    // clip-path: none !important;
+    font-size: 12px !important;
+    // padding: 0px 2px !important;
+    // border-radius: 3px !important;
   }
-  .total-price {
-    font-family: $fontfamliy3;
-    font-size: 15px;
-    color: $fontcolor !important;
-    pointer-events: none;
-    .befor-price {
-      font-family: sans-serif !important;
-      font-size: 13px;
-      font-weight: 700;
-      margin: 0 2px;
-      text-decoration: line-through;
-      color: grey !important;
-    }
-    .after-price {
-      font-family: sans-serif !important;
-      font-size: 13.5px;
-      font-weight: 700;
-      margin: 0 2px;
-      color: $fontcolor !important;
-    }
-    .save-price {
-      font-family: sans-serif !important;
-      font-size: 13.5px;
-      font-weight: 700;
-      margin: 0 2px;
-      color: $color-2 !important;
-    }
-  }
+}
+.befor-price {
+  font-family: sans-serif !important;
+  font-size: 13px;
+  font-weight: 700;
+  margin: 0 2px;
+  text-decoration: line-through;
+  color: grey !important;
+}
+.after-price {
+  font-family: sans-serif !important;
+  font-size: 13.5px;
+  font-weight: 700;
+  margin: 0 2px;
+  color: $fontcolor !important;
+}
+.save-price {
+  font-family: sans-serif !important;
+  font-size: 13.5px;
+  font-weight: 700;
+  margin: 0 2px;
+  color: $color-2 !important;
+}
+.total-price {
+  font-family: $fontfamliy3 !important;
+  font-size: 15px;
+  color: $fontcolor !important;
+  pointer-events: none;
 }
 .befor-price {
   font-family: sans-serif !important;
@@ -319,5 +489,26 @@ export default {
 ::v-deep .col-price.pa-2.col.col-2 {
   flex: 0 0 18%;
   max-width: 18%;
+}
+// Products
+.card-text {
+  font-family: $fontfamliy3;
+  letter-spacing: 0;
+  font-size: 14px;
+  color: $fontcolor !important;
+}
+
+.PriceAfter {
+  font-size: 17px !important;
+  display: block !important;
+  // color: $color-2;
+  font-weight: 600 !important;
+  font-family: sans-serif !important;
+}
+
+::v-deep .v-dialog.v-dialog--active.v-dialog--persistent {
+  margin: 5px;
+  // height: 95% !important;
+  border-radius: 0 !important;
 }
 </style>
